7cbafdad63901aa12a21ae3a5738a9440354e566
JatinSuthar07 <37161708+JatinSuthar07@users.noreply.github.com>
2018-04-11 14:55:37 -0700

    final code

--- jatin07.c
+++ jatin07.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
-
 struct round
 {
-int p,at,bt,wt,tat,ct,pr,st;
+int p,at,bt,wt,tat,ct,pr,st,f;
 }r[50];
-int tt=0,awt=0;
+float tt=0,awt=0;
 main()
 {
-int i,j,min,k,n,c,t,tq,b,a,rt[50];
+int i,j,min,b,k,n,c,t,tq,a,rt[50],minp,maxp,g=0;
 printf("\nEnter the no. of process ");
 scanf("\t%d",&n);
 c=n;
@@ -22,12 +21,18 @@ scanf("\t%d",&r[i].bt);
 rt[i]=r[i].bt;
 printf(" Priority   P%d :\t",i+1);
 scanf("\t%d",&r[i].pr);
+r[i].f=0;
+}
+printf("Q)");
+printf(" Process  \tAT\tBT\tPR");
+for(i=0;i<n;i++)
+{
+printf("\n     P%d  \t%d  \t%d  \t%d",i+1,r[i].at,r[i].bt,r[i].pr);
 }
 printf("\n\n\nEnter the time quantum");
 scanf("\t%d",&tq);
-
 min=r[0].at;
-
+minp=r[0].at;
 for(i= 1;i<n;i++)
 {
 	if(r[i].at<min)
@@ -38,32 +43,44 @@ for(i= 1;i<n;i++)
 	if(r[i].at==min)
 	{
 		r[i].st=1;
-	}
+	}	
 }
-printf("b");
+printf("\nProcess\t\tAT\tBT\tPR\tCT\tTAT\tWT");
 for(t=min;c!=0;)
-{
+{	
+
 for(i=0;i<n;i++)
 {
-	if(r[i].at<=t)
+	if(r[i].at<=t && r[i].st!=0)
 	{
 		r[i].st=1;
 	}
-	if(r[i].pr>a && r[i].st==1)
+	if(r[i].pr<=minp && r[i].st==1)
 	{
+		minp=r[i].pr;
+		b=i;
+	}
+	if(r[i].pr>=maxp && r[i].st==1)
+	{
+		maxp=r[i].pr;
 		a=i;
-	3}
+	}
 }
 if(rt[a]<=tq&&rt[a]>0)
 {
 	t=t+rt[a];
 	rt[a]=0;
 	r[a].st=1;
+	r[b].f=r[b].f+1;
+	r[a].f=0;
 }
 else if(rt[a]>0)
 {
 	rt[a]=rt[a]-tq;
 	t=t+tq;
+	if(r[a].f>0)
+		r[a].f=r[a].f-1;
+	r[b].f=r[b].f+1;
 }
 if(rt[a]==0 && r[a].st==1)
 {
@@ -73,10 +90,17 @@ if(rt[a]==0 && r[a].st==1)
 	r[a].wt=r[a].tat-r[a].bt;
 	tt=tt+r[a].tat;
 	awt=awt+r[a].wt;
-	printf("\nP%d\t%d\t%d\t%d\t%d\t%d\t%d",a+1,r[a].at,r[a].bt,r[a].pr,r[a].ct,r[a].tat,r[a].wt);
+	printf("\n  P%d \t\t%d  \t%d  \t%d  \t%d   \t%d  \t%d",a+1,r[a].at,r[a].bt,r[a].pr,r[a].ct,r[a].tat,r[a].wt);
 	r[a].st=0;
 }
+if(r[b].f>n && r[b].st==1)
+{
+	r[b].pr+=2;
+	r[b].f--;
 }
-printf("\n\naverage Turn-Tround-Time is \t%d",tt/n);
-printf("\naverage Waiting-Time is \t%d",awt/n);
+maxp=minp;
 }
+printf("\n\naverage Turn-Tround-Time is \t%0.2f",tt/n);
+printf("\naverage Waiting-Time is \t%0.2f",awt/n);
+}
+
